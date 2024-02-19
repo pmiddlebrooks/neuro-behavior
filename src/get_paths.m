7c275c3fbc7fb09278030a190fb597f1dd7562f9
Paul Middlebrooks <pmiddlebrooks@gmail.com>
2024-02-19 06:38:25 -0500

    auto-update

--- src/get_paths.m
+++ src/get_paths.m
@@ -1,25 +1,24 @@
-function paths = get_paths(computerDriveName)
+function paths = get_paths
 
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
 
-
-switch computerDriveName
-    case 'ROSETTA'
-            figurePath = 'E:/Projects/neuro-behavior/docs/';
+if exist('E:/Projects', 'dir')
+    figurePath = 'E:/Projects/neuro-behavior/docs/';
             bhvDataPath = 'E:/Projects/neuro-behavior/data/processed_behavior/';
             nrnDataPath = 'E:/Projects/neuro-behavior/data/raw_ephys/';
             saveDataPath = 'E:/Projects/neuro-behavior/data/';
-    case 'Z'
+elseif exist('Z:/middlebrooks/', 'dir')
             figurePath = 'Z:/middlebrooks/Projects/neuro-behavior/docs/';
             bhvDataPath = 'Z:/middlebrooks/Projects/neuro-behavior/data/processed_behavior/';
             nrnDataPath = 'Z:/middlebrooks/Projects/neuro-behavior/data/raw_ephys/';
             saveDataPath = 'Z:/middlebrooks/Projects/neuro-behavior/data/';
-    case 'home'
+elseif exist('/Users/paulmiddlebrooks/Projects/', 'dir')
             figurePath = '/Users/paulmiddlebrooks/Projects/neuro-behavior/docs/';
             bhvDataPath = '/Users/paulmiddlebrooks/Projects/neuro-behavior/data/processed_behavior/';
             nrnDataPath = '/Users/paulmiddlebrooks/Projects/neuro-behavior/data/raw_ephys/';
             saveDataPath = '/Users/paulmiddlebrooks/Projects/neuro-behavior/data/';
+           
 end
 if ~exist(figurePath, 'dir')
     mkdir(figurePath);
