573d6ef3a030e7da77050a29d7636be9255ec1b3
Paul Middlebrooks <pmiddlebrooks@gmail.com>
2024-04-09 17:07:21 -0400

    auto-update

--- src/kinematics_script.m
+++ src/kinematics_script.m
@@ -3,7 +3,7 @@ opts = neuro_behavior_options;
 opts.collectStart = 0 * 60 * 60; % seconds
 opts.collectFor = 2 * 60 * 60; % seconds
 opts.frameSize = 1/60;
-% opts.frameSize = .1;
+opts.frameSize = .1;
 
 getDataType = 'all';
 get_standard_data
@@ -18,9 +18,6 @@ end
     bhvDataPath = strcat(paths.bhvDataPath, 'animal_',animal,'/');
     bhvFileName = '2021-11-23_13-19-58DLC_resnet50_bottomup_clearSep21shuffle1_700000.csv';
 
-kinData = readtable([bhvDataPath, bhvFileName]);
-
-%%
 % Define the path to your CSV file
 csvFilePath = [bhvDataPath, bhvFileName];
 
@@ -45,7 +42,22 @@ end
 kinData = readtable(csvFilePath, opts);
 
 % Display the first few rows of the table to check
-disp(head(dataTable));
+disp(head(kinData));
+
+
+
+
+%%
+figure(43); clf; hold on;
+% plot(sqrt(kinData.left_forepaw_x(1:500).^2 +kinData.left_forepaw_y(1:500).^2))
+plot((kinData.left_forepaw_x(1:500) - kinData.snout_x(1:500)),  kinData.left_forepaw_y(1:500) - kinData.snout_y(1:500))
+plot((kinData.right_forepaw_x(1:500) - kinData.snout_x(1:500)),  kinData.right_forepaw_y(1:500) - kinData.snout_y(1:500))
+
+
+
+
+
+
 
 %%
 idx = sort([2:3:size(kinData, 2), 3:3:size(kinData, 2)]);
@@ -67,4 +79,49 @@ window = 5 * 60 * 60 + (1 : 360);
 figure(52); clf; hold on;
 for d = 1 : nDim
 plot(score(window, d));
+end
+
+
+
+
+
+
+
+
+%%
+nosePos = [kinData.snout_x, kinData.snout_y];
+tailPos = [kinData.tail_base_x, kinData.tail_base_y];
+leftPaw = [kinData.left_forepaw_x, kinData.left_forepaw_y];
+rightPaw = [kinData.right_forepaw_x, kinData.right_forepaw_y];
+
+[leftPawY, leftPawX] = calculateSignedDistances(nosePos, tailPos, leftPaw);
+[rightPawY, rightPawX] = calculateSignedDistances(nosePos, tailPos, rightPaw);
+
+
+plotWindow = 1 : 300;
+figure(43); clf; hold on;
+
+yline(0, '--k')
+plot(opts.frameSize * (0:length(plotWindow)-1), rightPawX(plotWindow), 'r')
+plot(opts.frameSize * (0:length(plotWindow)-1), leftPawX(plotWindow), 'b')
+
+
+function [distFromNose, distFromMidline] = calculateSignedDistances(nosePos, tailPos, thirdPoint)
+    % Calculate the vector from the nose to each third point
+    noseToThirdVec = thirdPoint - nosePos;
+    
+    % Calculate the unit vector for the line from the nose to the tail
+    lineVec = tailPos - nosePos;
+    unitLineVec = lineVec ./ sqrt(sum(lineVec.^2, 2));
+    
+    % Rotate the line vector by 90 degrees to get the perpendicular direction
+    perpUnitLineVec = [-unitLineVec(:,2), unitLineVec(:,1)];
+    
+    % Calculate the signed distance from the nose along the line
+    % This is the projection of the noseToThirdVec onto the line vector
+    distFromNose = sum(noseToThirdVec .* unitLineVec, 2);
+    
+    % Calculate the signed perpendicular distance
+    % This is the projection of the noseToThirdVec onto the perpendicular vector
+    distFromMidline = sum(noseToThirdVec .* perpUnitLineVec, 2);
 end
