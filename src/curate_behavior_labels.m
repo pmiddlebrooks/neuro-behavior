21f38c36eeb5dc2329aa33daef3321f6efa11ccb
Paul Middlebrooks <pmiddlebrooks@gmail.com>
2024-04-15 16:45:36 -0400

    auto-update

--- src/curate_behavior_labels.m
+++ src/curate_behavior_labels.m
@@ -37,13 +37,42 @@ end
 % Reclassify Valid behaviors based on new dataBhv
 dataBhv.Valid = behavior_selection(dataBhv, opts);
 
-% Create a new bhvIDMat based on the new dataBvh (same code as in
-% get_standard_data
-nFrame = ceil(opts.collectFor / opts.frameSize);
-bhvIDMat = int8(zeros(nFrame, 1));
-for i = 1 : size(dataBhv, 1)
-    iInd = dataBhv.StartFrame(i) : dataBhv.StartFrame(i) + dataBhv.DurFrame(i) - 1;
-    bhvIDMat(iInd) = dataBhv.ID(i);
-end
+
+    % Create bhvIDMat, a vector of ID labels, one element per frame to match the
+    % neural matrix
+
+    % 1. Create a vector of ID labels, one element per behavioral frame
+    % rate
+    timeWindows = 0 : 1/opts.fsBhv : dataBhv.StartTime(end) + dataBhv.Dur(end);
+    idFull = int8(zeros(length(timeWindows), 1));
+    for i = 1 : length(timeWindows) - 1
+        % Find behavior active during the current time window
+        idFull(i) = dataBhv.ID(dataBhv.StartTime <= timeWindows(i) & dataBhv.StartTime + dataBhv.Dur > timeWindows(i));
+    end
+    idFull(end) = dataBhv.ID(end);
+
+    % 2. Find majority behavior in each opts.frameSize window
+    nFrame = ceil(opts.collectFor / opts.frameSize);
+    bhvIDMat = int8(zeros(nFrame, 1));
+    for i = 1 : size(bhvIDMat, 1)
+        % if i == 100
+        %     disp('here')
+        % end
+        iStartTime = (i-1) * opts.frameSize;
+        iStopTime = i * opts.frameSize;
+        iStartFrame = round(1 + iStartTime * opts.fsBhv);
+        iStopFrame = round(iStopTime * opts.fsBhv);
+        iID = idFull(iStartFrame : iStopFrame);
+        bhvIDMat(i) = mode(iID);
+    end
+% 
+% % Create a new bhvIDMat based on the new dataBvh (same code as in
+% % get_standard_data
+% nFrame = ceil(opts.collectFor / opts.frameSize);
+% bhvIDMat = int8(zeros(nFrame, 1));
+% for i = 1 : size(dataBhv, 1)
+%     iInd = dataBhv.StartFrame(i) : dataBhv.StartFrame(i) + dataBhv.DurFrame(i) - 1;
+%     bhvIDMat(iInd) = dataBhv.ID(i);
+% end
 
 end
