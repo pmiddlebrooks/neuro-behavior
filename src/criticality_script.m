3bd5bbe5f518ff5a73418fab7ae5064961220ae3
Paul Middlebrooks <pmiddlebrooks@gmail.com>
2025-02-25 17:18:49 -0500

    auto-update

--- src/criticality_script.m
+++ src/criticality_script.m
@@ -2,14 +2,12 @@
 opts = neuro_behavior_options;
 opts.minActTime = .16;
 opts.collectStart = 0 * 60 * 60; % seconds
-opts.collectFor = 60 * 60; % seconds
+opts.collectFor = 30 * 60; % seconds
 
+paths = get_paths;
 %% Need to find an appropriate bin size for each brain area
-idSelect = idDS;
-opts.frameSize = .003;
-
-idSelect = idM56;
-opts.frameSize = .005;
+opts.frameSize = .005; % DS
+% opts.frameSize = .01; % M56
 
 opts.useOverlappingBins = 0;
 opts.windowSize = .2;
@@ -17,6 +15,9 @@ opts.stepSize = opts.frameSize;
 
 getDataType = 'spikes';
 get_standard_data
+
+idSelect = idDS;
+% idSelect = idM56;
 
 %%
 sumSpikes = sum(dataMat(:, idSelect), 2);
@@ -34,3 +35,164 @@ asdfMat = rastertoasdf2(dataMat(:,idSelect)', opts.frameSize*1000, 'CBModel', 'S
 Av = avprops(asdfMat, 'ratio', 'fingerprint');
 
 % Go to demoempdata.m to run rest of codes using Av
+
+
+
+
+
+
+
+
+
+
+
+
+%% Mark's reaching data
+opts.frameSize = .005; % DS
+opts.frameSize = .01; % DS
+opts.frameSize = .0075; % M56
+opts.minFiringRate = .1;
+
+data = load(fullfile(paths.saveDataPath, 'reach_data/Y4_100623_Spiketimes_idchan.mat'));
+[dataMat, idLabels, areaLabels, rmvNeurons] = neural_matrix_mark_data(data, opts);
+
+idM23 = find(strcmp(areaLabels, 'M23'));
+idM56 = find(strcmp(areaLabels, 'M56'));
+idDS = find(strcmp(areaLabels, 'DS'));
+idVS = find(strcmp(areaLabels, 'VS'));
+fprintf('%d M23\n%d M56\n%d DS\n%d VS\n', length(idM23), length(idM56), length(idDS), length(idVS))
+
+%%
+idSelect = idDS;
+idSelect = idM56;
+
+%
+sumSpikes = sum(dataMat(:, idSelect), 2);
+figure(91);
+histogram(sumSpikes, 'Normalization', 'pdf')
+% sum(sumSpikes == 0) / length(sumSpikes)
+
+
+%% go to demoempdata.m
+
+asdfMat = rastertoasdf2(dataMat(:,idSelect)', opts.frameSize*1000, 'CBModel', 'Spikes', 'DS')
+
+
+Av = avprops(asdfMat, 'ratio', 'fingerprint');
+
+% Go to demoempdata.m to run rest of codes using Av
+
+
+
+
+
+
+
+
+
+
+%% Set variables and get data
+opts.minFiringRate = .1;
+opts.collectFor = 45 * 60;
+getDataType = 'spikes';
+
+nIter = 20;
+nSubsample = 20;
+binSizes = [.005, .01, .025 .05];
+% binSizes = [.005, .05];
+% binSizes = [.005];
+
+% reach data
+dataR = load(fullfile(paths.saveDataPath, 'reach_data/Y4_100623_Spiketimes_idchan.mat'));
+
+brPeak = zeros(nIter, 2, length(binSizes));
+tau = brPeak;
+alpha = tau;
+sigmaNuZInvSD = tau;
+%%
+tic
+for b = 1 : length(binSizes)
+    for iter = 1:nIter
+        fprintf('\nIteration: %d\tBinSize: %.3f\t Time Elapsed: %.1f min\n', iter, binSizes(b), toc/60)
+
+        opts.frameSize = binSizes(b);
+
+
+        % Naturalistich data
+        get_standard_data
+
+        % Randomize a subsample of neurons
+        idSelect = idM56(randperm(length(idM56), nSubsample));
+
+
+        asdfMatM23 = rastertoasdf2(dataMat(:,idSelect)', opts.frameSize*1000, 'CBModel', 'Spikes', 'DS');
+        Av = avprops(asdfMatM23, 'ratio', 'fingerprint');
+
+        [brPeak(iter, 1, b), tau(iter, 1, b), alpha(iter, 1, b), sigmaNuZInvSD(iter, 1, b)] = avalanche_log(Av);
+
+
+        % % Mark's data
+        % [dataMatR, idLabels, areaLabels, rmvNeurons] = neural_matrix_mark_data(dataR, opts);
+        % % Ensure dataMatR is same size as dataMat
+        % dataMatR = dataMatR(1:size(dataMat, 1),:);
+        %
+        %
+        % idM23R = find(strcmp(areaLabels, 'M23'));
+        % idM56R = find(strcmp(areaLabels, 'M56'));
+        % idDSR = find(strcmp(areaLabels, 'DS'));
+        % idVSR = find(strcmp(areaLabels, 'VS'));
+        %
+        % % Randomize a subsample of neurons
+        % idSelect = idM56R(randperm(length(idM56R), nSubsample));
+        %
+        % asdfMatM23R = rastertoasdf2(dataMatR(:,idSelect)', opts.frameSize*1000, 'CBModel', 'Spikes', 'DS');
+        % AvR = avprops(asdfMatM23R, 'ratio', 'fingerprint');
+        %
+        % [brPeak(iter, 2, b), tau(iter, 2, b), alpha(iter, 2, b), sigmaNuZInvSD(iter, 2, b)] = avalanche_log(AvR);
+
+
+
+    end
+end
+
+
+function [brPeak, tau, alpha, sigmaNuZInvSD] = avalanche_log(Av)
+
+minBR = min(Av.branchingRatio);
+maxBR = max(Av.branchingRatio);
+
+nEdges = 25;
+edges = minBR:((maxBR-minBR)/(nEdges - 1)):maxBR;
+
+BRhist = histcounts(Av.branchingRatio, edges);
+[m, idx] = max(BRhist(2:end));
+brPeak = edges(idx+1);
+
+% size distribution (SZ)
+[tau, xminSZ, xmaxSZ, sigmaSZ, pSZ, pCritSZ, ksDR, DataSZ] =...
+    avpropvals(Av.size, 'size');
+% avpropvals(Av.size, 'size', 'plot');
+
+% size distribution (SZ) with cutoffs
+UniqSizes = unique(Av.size);
+Occurances = hist(Av.size,UniqSizes);
+AllowedSizes = UniqSizes(Occurances >= 20);
+AllowedSizes(AllowedSizes < 4) = [];
+LimSize = Av.size(ismember(Av.size,AllowedSizes));
+[tau, xminSZ, xmaxSZ, sigmaSZ, pSZ, pCritSZ, DataSZ] =...
+    avpropvals(LimSize, 'size');
+% avpropvals(LimSize, 'size', 'plot');
+tau = cell2mat(tau);
+
+% duration distribution (DR)
+[alpha, xminDR, xmaxDR, sigmaDR, pDR, pCritDR, ksDR, DataDR] =...
+    avpropvals(Av.duration, 'duration');
+% avpropvals(Av.duration, 'duration', 'plot');
+alpha = cell2mat(alpha);
+
+% size given duration distribution (SD)
+[sigmaNuZInvSD, waste, waste, sigmaSD] = avpropvals({Av.size, Av.duration},...
+    'sizgivdur', 'durmin', xminDR{1}, 'durmax', xmaxDR{1});
+% 'sizgivdur', 'durmin', xminDR{1}, 'durmax', xmaxDR{1}, 'plot');
+end
+
---
+++ src/neural_matrix_mark_data.m
@@ -0,0 +1,169 @@
+function [dataMat, idLabels, areaLabels, rmvNeurons] = neural_matrix_mark_data(data, opts)
+% NEURAL_MATRIX_MARK_DATA - Converts spike time data into a binned matrix representation
+%
+% This function takes in a data structure containing spike times, spike IDs,
+% and behavior durations, and bins the spikes into a matrix based on the specified
+% options. It allows both non-overlapping binning and an optional sliding window
+% approach with overlapping bins.
+%
+% INPUTS:
+%   data - Structure containing the following fields:
+%       .spikeTimes    - Vector of spike timestamps
+%       .spikeClusters - Vector of neuron IDs corresponding to each spike
+%       .ci            - Table containing neuron metadata (id, area, etc.)
+%   opts - Structure containing the following options:
+%       .method          - Method of creating dataMat: 'standard', 'useOverlap', or 'gaussian'
+%       .frameSize       - Bin size for final dataMat
+%       .shiftAlignFactor - Time shift applied to spike bins (default: 0)
+%       .useNeurons      - List of neuron IDs to include
+%       .removeSome      - Flag to filter out neurons based on firing rate
+%       .minFiringRate   - Minimum firing rate threshold (for removal if enabled)
+%       .maxFiringRate   - Maximum firing rate threshold (for removal if enabled)
+%       For method = 'useOverlap'
+%       .windowSize      - Window size for summing spikes in overlapping binning
+%       .stepSize        - Step size for shifting the sliding window
+%       .windowAlign     - Window alignment ('center', 'left', or 'right', default: 'center')
+%       For method = 'gaussian'
+%       .gaussWidth      - if method is gaussian, width of the kernel in ms
+%
+% OUTPUTS:
+%   dataMat     - Binned spike matrix (time bins x neurons)
+%   idLabels    - List of neuron IDs included in dataMat
+%   areaLabels  - List of brain area labels for included neurons
+%   rmvNeurons  - Logical vector indicating neurons removed based on firing rate criteria
+%
+
+
+if opts.shiftAlignFactor ~= 0
+    warning('neural_matrix.m: You shifted the spike bin time w.r.t. the behavior start time: make sure you wanted to do this.')
+end
+
+% Determine total duration in frames
+lastSecond = ceil(max(data.CSV(:,1)));
+numFrames = ceil(lastSecond / opts.frameSize);
+
+useNeurons = find(data.idchan(:,end) ~= 0); % Keep everything that isn't corpus collosum
+idLabels = data.idchan(useNeurons, 1);
+brainAreas = data.idchan(useNeurons, end);
+areaLabels = cell(size(brainAreas));
+areaLabels(brainAreas == 1) = {'M23'};
+areaLabels(brainAreas == 2) = {'M56'};
+areaLabels(brainAreas == 3) = {'DS'};
+areaLabels(brainAreas == 4) = {'VS'};
+
+% Preallocate data matrix
+if numFrames > 30 * 60 / 0.001
+    dataMat = int8(zeros(numFrames, length(idLabels)));
+else
+    dataMat = zeros(numFrames, length(idLabels));
+end
+
+if strcmp(opts.method, 'useOverlap')
+    warning('You are using overlapping bins, so dataMat is in firing rate units (sp/s)')
+    % Define window and step size
+    windowSize = opts.windowSize; % Summing window size
+    stepSize = opts.stepSize; % Step size for shifting the window
+    if ~isfield(opts, 'windowAlign')
+        opts.windowAlign = 'center'; % Default alignment
+    end
+end
+if strcmp(opts.method, 'gaussian')
+    totalTimeMs = ceil(sum(data.bhvDur)*1000); % Ensure time resolution in ms
+
+    % Define Gaussian kernel for convolution
+    kernelRange = round(5 * opts.gaussWidth); % 5 SD range ensures near-zero tails
+    timeVec = -kernelRange:kernelRange;
+    gaussKernel = exp(-timeVec.^2 / (2 * opts.gaussWidth^2));
+    gaussKernel = gaussKernel / sum(gaussKernel); % Normalize
+
+    % Create high-resolution firing rate matrix (ms resolution)
+    spikeRateMs = zeros(totalTimeMs, length(idLabels));
+end
+
+% Loop through each neuron and bin spike counts
+for i = 1:length(idLabels)
+    iSpikeTime = data.CSV(data.CSV(:,2) == idLabels(i), 1);
+
+    switch opts.method
+        case 'useOverlap'
+            % Overlapping bin edges
+            timeEdges = 0:stepSize:lastSecond; % Define steps
+            dataMatTemp = zeros(length(timeEdges) - 1, 1);
+
+            % Apply sliding window approach
+            for j = 1:length(timeEdges)-1
+                switch opts.windowAlign
+                    case 'center'
+                        winStart = timeEdges(j) - windowSize / 2;
+                        winEnd = timeEdges(j) + windowSize / 2;
+                    case 'left'
+                        winStart = timeEdges(j);
+                        winEnd = timeEdges(j) + windowSize;
+                    case 'right'
+                        winStart = timeEdges(j) - windowSize;
+                        winEnd = timeEdges(j);
+                end
+
+                % Count spikes in the current window
+                % dataMatTemp(j) = sum(iSpikeTime >= winStart & iSpikeTime < winEnd);
+                dataMatTemp(j) = sum(iSpikeTime >= winStart & iSpikeTime < winEnd) ./ windowSize;
+            end
+
+            % Store in dataMat
+            dataMat(:, i) = dataMatTemp;
+        case 'standard'
+            % Standard non-overlapping binning
+            timeEdges = 0:opts.frameSize:lastSecond;
+            [iSpikeCount, ~] = histcounts(iSpikeTime, timeEdges);
+            dataMat(:, i) = iSpikeCount';
+        case 'gaussian'
+            % Convert spike times to binary spike train
+            spikeTrain = zeros(totalTimeMs, 1);
+            spikeTrain(round(iSpikeTime * 1000)) = 1;
+
+            % Convolve with Gaussian kernel to get smoothed firing rate
+            smoothedRate = conv(spikeTrain, gaussKernel, 'same'); % divide by 1000 to get smoothed rate
+            spikeRateMs(:, i) = smoothedRate * opts.fsSpike; % Convert to Hz
+            spikeRateMs(:, i) = smoothedRate * 1000; % Convert to Hz
+
+    end
+
+end
+
+    if strcmp(opts.method, 'gaussian')
+         % Loop through neurons and generate spike rate vectors
+           for b = 1:numFrames
+                realTimeStart = (b-1) * opts.frameSize * 1000;
+                binStart = round(realTimeStart) + 1;
+                binEnd = min(round(realTimeStart + opts.frameSize * 1000), totalTimeMs);
+                dataMat(b, :) = mean(spikeRateMs(binStart:binEnd, :), 1);
+            end
+    end
+
+
+% Remove neurons that do not meet firing rate criteria
+rmvNeurons = [];
+if opts.removeSome
+    checkTime = 5 * 60;
+    checkFrames = floor(checkTime / opts.frameSize);
+    if ~strcmp(opts.method, 'standard')
+        meanStart = mean(dataMat(1:checkFrames, :), 1);
+        meanEnd = mean(dataMat(end-checkFrames+1:end, :), 1);
+    else
+        meanStart = sum(dataMat(1:checkFrames, :), 1) ./ checkTime;
+        meanEnd = sum(dataMat(end-checkFrames+1:end, :), 1) ./ checkTime;
+    end
+    keepStart = meanStart >= opts.minFiringRate & meanStart <= opts.maxFiringRate;
+    keepEnd = meanEnd >= opts.minFiringRate & meanEnd <= opts.maxFiringRate;
+    rmvNeurons = ~(keepStart & keepEnd);
+    fprintf('\nkeeping %d of %d neurons\n', sum(~rmvNeurons), length(rmvNeurons));
+    dataMat(:, rmvNeurons) = [];
+    idLabels(rmvNeurons) = [];
+    areaLabels(rmvNeurons) = [];
+end
+end
+
+
+
+
+
