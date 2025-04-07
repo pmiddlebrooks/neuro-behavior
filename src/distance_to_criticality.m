3afb61f855d9aa2ed43115f733242627e7301dbd
Paul Middlebrooks <pmiddlebrooks@gmail.com>
2025-04-07 16:02:32 -0400

    auto-update

---
+++ src/distance_to_criticality.m
@@ -0,0 +1,22 @@
+function dcc = distance_to_criticality(alphaFitted, tauFitted, gammaFitted)
+    % computeDCC calculates the Distance to Criticality Coefficient
+    % from Ma et al 2019
+    %
+    % INPUTS:
+    %   alpha_fitted  - avalanche size exponent
+    %   tau_fitted    - avalanche duration exponent
+    %   gamma_fitted  - exponent from size-duration scaling
+    % OUTPUT:
+    %   dcc - distance to criticality coefficient
+
+    % Check for valid input to avoid division by zero
+    if tauFitted <= 1
+        error('tau_fitted must be > 1 to compute gamma_predicted');
+    end
+
+    % Compute predicted gamma from exponent relation
+    gammaPredicted = (alphaFitted - 1) / (tauFitted - 1);
+
+    % Compute distance to criticality
+    dcc = abs(gammaPredicted - gammaFitted);
+end
