ebfc7652fbffbdc9f0783294769cb91ac3847c00
Paul Middlebrooks <pmiddlebrooks@gmail.com>
2025-04-24 11:40:20 -0400

    auto-update

--- src/criticality/criticality_shew.m
+++ src/criticality/criticality_shew.m
@@ -35,28 +35,41 @@ monitorOne = monitorPositions(1, :); % Just use single monitor if you don't have
 monitorTwo = monitorPositions(size(monitorPositions, 1), :); % Just use single monitor if you don't have second one
 
 
-%%   ====================================     Mark Task: Block1 vs Block2       ==============================================
+
+%% Naturalistic data
 opts = neuro_behavior_options;
 opts.frameSize = .001;
 opts.minFiringRate = .1;
 getDataType = 'spikes';
-opts.collectStart = 2 * 60 * 60; % seconds
+opts.collectStart = 0 * 60 * 60; % seconds
 opts.collectFor = 2 * 60 * 60;
 opts.firingRateCheckTime = 5 * 60;
 get_standard_data
+
+areas = {'M23', 'M56', 'DS', 'VS'};
+idList = {idM23, idM56, idDS, idVS};
 
 %% Mark's reach data
 dataR = load(fullfile(paths.dropPath, 'reach_data/Y4_100623_Spiketimes_idchan_BEH.mat'));
 [dataMatR, idLabels, areaLabels, rmvNeurons] = neural_matrix_mark_data(dataR, opts);
 % Ensure dataMatR is same size as dataMat
-dataMatR = dataMatR(1:size(dataMat, 1),:);
 idM23R = find(strcmp(areaLabels, 'M23'));
 idM56R = find(strcmp(areaLabels, 'M56'));
 idDSR = find(strcmp(areaLabels, 'DS'));
 idVSR = find(strcmp(areaLabels, 'VS'));
 
+areas = {'M23', 'M56', 'DS', 'VS'};
+idList = {idM23R, idM56R, idDSR, idVSR};
 
-%%
+
+
+
+
+
+
+
+%%   ====================================     Mark Task: Block1 vs Block2       ==============================================
+
 areas = {'M23', 'M56', 'DS', 'VS'};
 idList = {idM23R, idM56R, idDSR, idVSR};
 
@@ -108,10 +121,8 @@ end
 isiMult = 10; % Multiple of mean ISI to determine minimum bin size
 pOrder = 10; % Order parameter for the autoregressor model
 critType = 2;
-areas = {'M23', 'M56', 'DS', 'VS'};
 [optBinSize, d2N, d2NR, varNoiseN, varNoiseNR] = ...
     deal(nan(length(areas), 1));
-idList = {idM23, idM56, idDS, idVS};
 
 
 for a = 1 : length(areas)
@@ -154,6 +165,209 @@ title('Distance to Criticality')
 xticks(1:4)
 xticklabels({'M23', 'M56', 'DS', 'VS'})
 ylabel('Distance to criticality')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%%   ====================================     Mark Task: Sliding window       ==============================================
+
+
+isiMult = 10; % Multiple of mean ISI to determine minimum bin size
+pOrder = 10; % Order parameter for the autoregressor model
+critType = 2;
+
+% Define sliding window parameters
+windowSize = 2 * 60; % 10 minutes (in seconds)
+stepSize = 1 * 60; % 2 minutes (in seconds)
+Fs = 1000; % dataR is in ms
+
+% Convert window and step size to samples
+winSamples = round(windowSize * Fs);
+stepSamples = round(stepSize * Fs);
+numTimePoints = size(dataMatR, 1);
+
+% Preallocate or store outputs if needed
+numWindows = floor((numTimePoints - winSamples) / stepSamples) + 1;
+
+
+% Initialize variables
+optBinSize = nan(length(areas), 1);
+[d2,d2R, varNoise, varNoiseR] = ...
+    deal(nan(numWindows, length(areas)));
+startMin = nan(numWindows, 1);
+
+trialIdx2 = ismember(dataR.block(:, 3), 3:4);
+block2First = find(trialIdx2, 1);
+block2Start = dataR.R(block2First, 1);
+block2StartMin = block2Start / Fs / 60;
+
+for a = 1 : length(areas)
+    fprintf('Area %s\n', areas{a})
+    tic
+    aID = idList{a};
+    optBinSize(a) = isiMult * round(mean(diff(find(sum(dataMatR(:, aID), 2))))) / 1000;
+% optBinSize(a) = .05;
+
+for w = 1:numWindows
+    startIdx = (w - 1) * stepSamples + 1;
+    endIdx = startIdx + winSamples - 1;
+
+    startMin(w) = startIdx / 1000 / 60;
+    popActivity = neural_matrix_ms_to_frames(sum(dataMatR(startIdx:endIdx, aID), 2), optBinSize(a));
+
+    [varphi, varNoise(w,a)] = myYuleWalker3(popActivity, pOrder);
+    d2(w,a) =getFixedPointDistance2(pOrder, critType, varphi);
+
+    popActivityR = popActivity(randperm(length(popActivity)));
+
+    [varphi, varNoiseR(w,a)] = myYuleWalker3(popActivityR, pOrder);
+    d2R(w,a) =getFixedPointDistance2(pOrder, critType, varphi);
+
+end
+toc
+end
+% [d21 d21R d22 d22R]
+%%
+figure(51); clf; hold on;
+plot(startMin, d2(:,1), 'ok', 'lineWidth', 2);
+plot(startMin, d2(:,2), 'ob', 'lineWidth', 2);
+plot(startMin, d2(:,3), 'or', 'lineWidth', 2);
+plot(startMin, d2(:,4), 'og', 'lineWidth', 2);
+plot(startMin, d2R(:,1), '*k');
+plot(startMin, d2R(:,2), '*b');
+plot(startMin, d2R(:,3), '*r');
+plot(startMin, d2R(:,4), '*g');
+xline(block2StartMin)
+legend({'M23', 'M56', 'DS', 'VS'}, 'Location','northwest')
+xlabel('Minutes')
+ylabel('Distance to criticality')
+
+
+
+
+
+
+
+
+
+
+%%   ====================================     Natrualistic: Sliding window       ==============================================
+
+
+isiMult = 10; % Multiple of mean ISI to determine minimum bin size
+pOrder = 10; % Order parameter for the autoregressor model
+critType = 2;
+
+% Define sliding window parameters
+windowSize = 5 * 60; % 10 minutes (in seconds)
+stepSize = 1 * 60; % 2 minutes (in seconds)
+Fs = 1/opts.frameSize; % dataR is in ms
+
+% Convert window and step size to samples
+winSamples = round(windowSize * Fs);
+stepSamples = round(stepSize * Fs);
+numTimePoints = size(dataMat, 1);
+
+% Preallocate or store outputs if needed
+numWindows = floor((numTimePoints - winSamples) / stepSamples) + 1;
+
+
+% Initialize variables
+optBinSize = nan(length(areas), 1);
+[d2,d2R, varNoise, varNoiseR] = ...
+    deal(nan(numWindows, length(areas)));
+startMin = nan(numWindows, 1);
+
+for a = 1 : length(areas)
+    fprintf('Area %s\n', areas{a})
+    tic
+    aID = idList{a};
+    optBinSize(a) = isiMult * round(mean(diff(find(sum(dataMat(:, aID), 2))))) / 1000;
+% optBinSize(a) = .05;
+
+for w = 1:numWindows
+    startIdx = (w - 1) * stepSamples + 1;
+    endIdx = startIdx + winSamples - 1;
+
+    startMin(w) = startIdx / 1000 / 60;
+    popActivity = neural_matrix_ms_to_frames(sum(dataMat(startIdx:endIdx, aID), 2), optBinSize(a));
+
+    [varphi, varNoise(w,a)] = myYuleWalker3(popActivity, pOrder);
+    d2(w,a) =getFixedPointDistance2(pOrder, critType, varphi);
+
+    popActivityR = popActivity(randperm(length(popActivity)));
+
+    [varphi, varNoiseR(w,a)] = myYuleWalker3(popActivityR, pOrder);
+    d2R(w,a) =getFixedPointDistance2(pOrder, critType, varphi);
+
+end
+toc
+end
+% [d21 d21R d22 d22R]
+%
+figure(51); clf; hold on;
+plot(startMin, d2(:,1), 'ok', 'lineWidth', 2);
+plot(startMin, d2(:,2), 'ob', 'lineWidth', 2);
+plot(startMin, d2(:,3), 'or', 'lineWidth', 2);
+plot(startMin, d2(:,4), 'og', 'lineWidth', 2);
+plot(startMin, d2R(:,1), '*k');
+plot(startMin, d2R(:,2), '*b');
+plot(startMin, d2R(:,3), '*r');
+plot(startMin, d2R(:,4), '*g');
+xline(block2StartMin)
+legend({'M23', 'M56', 'DS', 'VS'}, 'Location','northwest')
+xlabel('Minutes')
+ylabel('Distance to criticality')
+title('Naturalistic')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
