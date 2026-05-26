7af4c808ab59d16d79d41d5886722556ca2ad519
Paul Middlebrooks <pmiddlebrooks@gmail.com>
2026-05-26 17:24:47 -0400

    from work

--- src/data_prep/choose_task_and_session.m
+++ src/data_prep/choose_task_and_session.m
@@ -68,12 +68,13 @@ sessionName =  'ey9166_2026_04_03';
 % sessionName =  'ey9166_2026_04_07';
 % sessionName =  'ey9166_2026_04_09';
 % 
-% subjectName = 'ey9387';
+subjectName = 'ey9387';
 % sessionName =  'ey9387_2026_05_19';
 % sessionName =  'ey9387_2026_05_20';
 % sessionName =  'ey9387_2026_05_21';
-% sessionName =  'ey9387_2026_05_22';
+sessionName =  'ey9387_2026_05_22';
 % sessionName =  'ey9387_2026_05_25';
+sessionName =  'ey9387_2026_05_26';
 
 %% =============================    Reach Data Loading    =============================
 sessionType = 'reach';  % 'reach' , 'spontaneous' , 'schall' , 'hong'
